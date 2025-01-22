086fd51d55208b78edbff8c2fe3ec6513ca320e3
Josh Jarabek <josh@parakeet.ventures>
2025-01-21 18:25:48 -0600

    Removed linters

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,10 @@
-import path from 'path';
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  experimental: {
-    esmExternals: true,
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.join(__dirname),
-    };
-    return config;
-  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
